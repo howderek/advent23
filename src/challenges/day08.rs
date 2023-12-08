16f4f86fe861aa173cf4774975fa7881cf966072
Derek Howard <derek@howderek.com>
2023-12-08 12:57:07 -0600

    feat: use fromstr trait for day 8

--- src/challenges/day08.rs
+++ src/challenges/day08.rs
@@ -1,6 +1,6 @@
 use advent23::vendor::lcm;
 use clap;
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fs, str::FromStr};
 
 #[derive(clap::Args, Debug)]
 pub struct Args {
@@ -17,86 +17,82 @@ pub enum Instruction {
     Right,
 }
 
-pub fn parse_tree(s: &str) -> HashMap<&str, (&str, &str)> {
-    let mut map: HashMap<&str, (&str, &str)> = HashMap::new();
-    for line in s.lines() {
-        let id = &line[0..3];
-        let left = &line[7..10];
-        let right = &line[12..15];
-        map.insert(id, (left, right));
-    }
-    map
+struct Program {
+    graph: HashMap<String, (String, String)>,
+    stack: Vec<Instruction>,
 }
 
-pub fn parse_traversal_stack(s: &str) -> Vec<Instruction> {
-    s.chars()
-        .rev()
-        .flat_map(|c| match c {
-            'L' => Some(Instruction::Left),
-            'R' => Some(Instruction::Right),
-            _ => None,
-        })
-        .collect()
-}
-
-pub fn follow_path(
-    starts: Vec<&str>,
-    tree: &HashMap<&str, (&str, &str)>,
-    stack: &Vec<Instruction>,
-) -> u64 {
-    let mut step_count: u64 = 0;
-    let mut nodes: Vec<&str> = starts.clone();
-    loop {
-        let mut s: Vec<Instruction> = stack.clone();
-        while let Some(next) = s.pop() {
-            if nodes.iter().all(|node| node.ends_with("Z")) {
-                return step_count;
-            }
-            nodes.iter_mut().for_each(|node| {
-                let (left, right) = tree.get(node).unwrap();
-                match next {
-                    Instruction::Left => {
-                        *node = left;
-                    }
-                    Instruction::Right => {
-                        *node = right;
-                    }
+impl Program {
+    pub fn run(&self, start: String) -> u64 {
+        let mut step_count: u64 = 0;
+        let mut node = &start;
+        loop {
+            let mut s: Vec<Instruction> = self.stack.clone();
+            while let Some(next) = s.pop() {
+                if node.ends_with("Z") {
+                    return step_count;
                 }
-            });
-            step_count += 1;
+                let (left, right) = self.graph.get(node).unwrap();
+                match next {
+                    Instruction::Left => node = left,
+                    Instruction::Right => node = right,
+                }
+                step_count += 1;
+            }
         }
     }
 }
 
-pub fn part1(args: &Args) -> u64 {
-    let input = fs::read_to_string(&args.file).expect("I/O error");
-    let parts: Vec<&str> = input.split("\n\n").collect();
-    let orig_stack = parse_traversal_stack(parts[0]);
-    let tree = parse_tree(parts[1]);
-    follow_path(vec!["AAA"], &tree, &orig_stack)
+#[derive(Debug, PartialEq, Eq)]
+struct ParseProgramError;
+
+impl FromStr for Program {
+    type Err = ParseProgramError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split("\n\n").collect();
+        // Parse the instructions
+        let stack: Vec<Instruction> = parts[0]
+            .chars()
+            .rev()
+            .flat_map(|c| match c {
+                'L' => Some(Instruction::Left),
+                'R' => Some(Instruction::Right),
+                _ => None,
+            })
+            .collect();
+        // Parse the tree;
+        let mut graph: HashMap<String, (String, String)> = HashMap::new();
+        for line in parts[1].lines() {
+            let id = line[0..3].to_string();
+            let left = line[7..10].to_string();
+            let right = line[12..15].to_string();
+            graph.insert(id, (left, right));
+        }
+        Ok(Program { graph, stack })
+    }
 }
 
-pub fn part2(args: &Args) -> u64 {
-    let input = fs::read_to_string(&args.file).expect("I/O error");
-    let parts: Vec<&str> = input.split("\n\n").collect();
-    let orig_stack = parse_traversal_stack(parts[0]);
-    let tree = parse_tree(parts[1]);
-    let starts: Vec<&str> = tree
+pub fn part1(input: String) -> u64 {
+    input.parse::<Program>().unwrap().run("AAA".to_string())
+}
+
+pub fn part2(input: String) -> u64 {
+    let program: Program = input.parse().unwrap();
+    lcm(program
+        .graph
         .iter()
-        .filter(|(key, (_, _))| key.ends_with("A"))
-        .map(|(id, _)| *id)
-        .collect();
-    let factors: Vec<u64> = starts
-        .iter()
-        .map(|k| follow_path(vec![k], &tree, &orig_stack))
-        .collect();
-    lcm(factors.as_slice())
+        .filter_map(|(key, (_, _))| if key.ends_with("A") { Some(key) } else { None })
+        .map(|start| program.run(start.clone()))
+        .collect::<Vec<_>>()
+        .as_slice())
 }
 
 pub fn entrypoint(args: &Args) {
+    let input = fs::read_to_string(&args.file).expect("I/O error");
     if args.part2 {
-        println!("{}", part2(args));
+        println!("{}", part2(input));
     } else {
-        println!("{}", part1(args));
+        println!("{}", part1(input));
     }
 }
