8d8f174f9abe1048c37f0af408e162075e1cee1c
Derek Howard <derek@howderek.com>
2023-12-10 02:40:24 -0600

    feat: day 10

--- src/challenges/day09.rs
+++ src/challenges/day09.rs
@@ -1,3 +1,4 @@
+use advent23::parse_number_list;
 use clap;
 use std::{fs, str};
 
@@ -10,7 +11,85 @@ pub struct Args {
     part2: bool,
 }
 
+pub fn derivative(v: &Vec<i64>) -> Vec<i64> {
+    let mut result = Vec::new();
+    for i in 0..v.len() - 1 {
+        result.push(v[i + 1] - v[i]);
+    }
+    result
+}
+
+pub fn autoderive(v: &Vec<i64>) -> Vec<Vec<i64>> {
+    let mut result = vec![v.clone()];
+    let mut next = v.clone();
+    while !next.iter().all(|x| *x == 0) {
+        next = derivative(&next);
+        result.push(next.clone());
+    }
+    result
+}
+
+pub fn extrapolate(v: &mut Vec<Vec<i64>>) -> &mut Vec<Vec<i64>> {
+    {
+        let len = v.len();
+        let base: &mut Vec<i64> = &mut v[len - 1];
+        base.push(0);
+    }
+    for i in 1..v.len() {
+        let level_idx = v.len() - i - 1;
+        {
+            let last_lower_level = v[level_idx + 1][v[level_idx + 1].len() - 1];
+            let level: &mut Vec<i64> = &mut v[level_idx];
+            level.push(level[level.len() - 1] + last_lower_level);
+        }
+    }
+    v
+}
+
+pub fn extrapolate_left(v: &mut Vec<Vec<i64>>) -> &mut Vec<Vec<i64>> {
+    {
+        let len = v.len();
+        let base: &mut Vec<i64> = &mut v[len - 1];
+        base.push(0);
+    }
+    for i in 1..v.len() {
+        let level_idx = v.len() - i - 1;
+        {
+            let first_lower_level = v[level_idx + 1][0];
+            let level: &mut Vec<i64> = &mut v[level_idx];
+            level.insert(0, level[0] - first_lower_level);
+        }
+    }
+    v
+}
+
+pub fn part1(input: String) -> i64 {
+    let mut result = 0;
+    let sequences: Vec<Vec<i64>> = input.lines().map(|l| parse_number_list(l)).collect();
+    for sequence in sequences.iter() {
+        let mut layers = autoderive(&sequence);
+        extrapolate(&mut layers);
+        result += layers[0][layers[0].len() - 1];
+    }
+    result
+}
+
+pub fn part2(input: String) -> i64 {
+    let mut result = 0;
+    let sequences: Vec<Vec<i64>> = input.lines().map(|l| parse_number_list(l)).collect();
+    for sequence in sequences.iter() {
+        let mut layers = autoderive(&sequence);
+        extrapolate_left(&mut layers);
+        result += layers[0][0];
+    }
+    result
+}
+
 pub fn entrypoint(args: &Args) {
-    let _input = fs::read_to_string(&args.file).expect("I/O error");
-    println!("this day has not yet been implemented")
+    let input = fs::read_to_string(&args.file).expect("I/O error");
+    if args.part2 {
+        println!("{}", part2(input));
+    } else {
+        println!("{}", part1(input));
+    }
 }
--- src/challenges/day10.rs
+++ src/challenges/day10.rs
@@ -1,16 +1,367 @@
 use clap;
-use std::{fs, str};
+use std::{
+    collections::{HashSet, VecDeque},
+    fs, str,
+};
 
 #[derive(clap::Args, Debug)]
 pub struct Args {
     #[arg(default_value_t = String::from("./inputs/day10/input.txt"))]
     file: String,
-
+    /// Find "gear ratios"
     #[clap(long, short, action)]
     part2: bool,
 }
 
+fn string_as_2d_array(s: String) -> Vec<Vec<char>> {
+    s.lines().map(|l| l.chars().collect()).collect()
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Tile {
+    x: usize,
+    y: usize,
+    w: usize,
+    h: usize,
+}
+
+impl Tile {
+    pub fn new(world_width: usize, world_height: usize, x: usize, y: usize) -> Self {
+        Tile {
+            x,
+            y,
+            w: world_width,
+            h: world_height,
+        }
+    }
+
+    pub fn from_world(world: &Vec<Vec<char>>, x: usize, y: usize) -> Self {
+        Tile {
+            x,
+            y,
+            w: world[0].len(),
+            h: world.len(),
+        }
+    }
+
+    pub fn char_at(&self, world: &Vec<Vec<char>>) -> Option<char> {
+        if self.y >= world.len() || self.x >= world[self.y].len() {
+            None
+        } else {
+            Some(world[self.y][self.x])
+        }
+    }
+
+    pub fn digit_at(&self, world: &Vec<Vec<char>>) -> Option<u32> {
+        if self.y >= world.len() || self.x >= world[self.y].len() {
+            None
+        } else {
+            world[self.y][self.x].to_digit(10)
+        }
+    }
+
+    pub fn topleft(&self) -> Option<Self> {
+        if self.x > 0 && self.y > 0 {
+            return Some(Self::new(self.w, self.h, self.x - 1, self.y - 1));
+        }
+        return None;
+    }
+
+    pub fn top(&self) -> Option<Self> {
+        if self.y > 0 {
+            return Some(Self::new(self.w, self.h, self.x, self.y - 1));
+        }
+        return None;
+    }
+
+    pub fn topright(&self) -> Option<Self> {
+        if self.x + 1 < self.w && self.y > 0 {
+            return Some(Self::new(self.w, self.h, self.x + 1, self.y - 1));
+        }
+        return None;
+    }
+
+    pub fn left(&self) -> Option<Self> {
+        if self.x > 0 {
+            return Some(Self::new(self.w, self.h, self.x - 1, self.y));
+        }
+        return None;
+    }
+
+    pub fn right(&self) -> Option<Self> {
+        if self.x + 1 < self.w {
+            return Some(Self::new(self.w, self.h, self.x + 1, self.y));
+        }
+        return None;
+    }
+
+    pub fn bottomleft(&self) -> Option<Self> {
+        if self.x > 0 && self.y + 1 < self.h {
+            return Some(Self::new(self.w, self.h, self.x - 1, self.y + 1));
+        }
+        return None;
+    }
+
+    pub fn bottom(&self) -> Option<Self> {
+        if self.y + 1 < self.h {
+            return Some(Self::new(self.w, self.h, self.x, self.y + 1));
+        }
+        return None;
+    }
+
+    pub fn bottomright(&self) -> Option<Self> {
+        if self.x + 1 < self.w && self.y + 1 < self.h {
+            return Some(Self::new(self.w, self.h, self.x + 1, self.y + 1));
+        }
+        return None;
+    }
+
+    pub fn adjacencies(&self) -> Vec<Self> {
+        [self.top(), self.left(), self.right(), self.bottom()]
+            .into_iter()
+            .flatten()
+            .collect()
+    }
+
+    pub fn is_connected(&self, world: &Vec<Vec<char>>, other: &Self) -> bool {
+        let c = other.char_at(world);
+        if other.x == self.x && other.y == self.y + 1 {
+            return c == Some('|') || c == Some('L') || c == Some('J');
+        } else if other.x == self.x && self.y > 0 && other.y == self.y - 1 {
+            return c == Some('|') || c == Some('7') || c == Some('F') || c == Some('S');
+        } else if self.x > 0 && other.x == self.x - 1 && other.y == self.y {
+            return c == Some('-') || c == Some('F') || c == Some('L') || c == Some('S');
+        } else if other.x == self.x + 1 && other.y == self.y {
+            return c == Some('-') || c == Some('J') || c == Some('7') || c == Some('S');
+        }
+        return false;
+    }
+
+    pub fn neighbors(&self, world: &Vec<Vec<char>>) -> Vec<Self> {
+        match self.char_at(world) {
+            Some('|') => [self.top(), self.bottom()]
+                .into_iter()
+                .flatten()
+                .filter(|x| self.is_connected(world, x))
+                .collect(),
+            Some('-') => [self.left(), self.right()]
+                .into_iter()
+                .flatten()
+                .filter(|x| self.is_connected(world, x))
+                .collect(),
+            Some('L') => [self.top(), self.right()]
+                .into_iter()
+                .flatten()
+                .filter(|x| self.is_connected(world, x))
+                .collect(),
+            Some('J') => [self.top(), self.left()]
+                .into_iter()
+                .flatten()
+                .filter(|x| self.is_connected(world, x))
+                .collect(),
+            Some('7') => [self.bottom(), self.left()]
+                .into_iter()
+                .flatten()
+                .filter(|x| self.is_connected(world, x))
+                .collect(),
+            Some('F') => [self.bottom(), self.right()]
+                .into_iter()
+                .flatten()
+                .filter(|x| self.is_connected(world, x))
+                .collect(),
+            Some('S') => self
+                .adjacencies()
+                .into_iter()
+                .filter(|x| self.is_connected(world, x))
+                .collect(),
+            _ => vec![],
+        }
+    }
+}
+
+fn follow_pipe(world: &Vec<Vec<char>>, x: usize, y: usize) -> Vec<(Tile, u64)> {
+    let mut stack: VecDeque<(Tile, u64)> = [(Tile::from_world(world, x, y), 0)].into();
+    let mut visited: HashSet<Tile> = HashSet::new();
+    let mut result: Vec<(Tile, u64)> = vec![];
+    while let Some((tile, distance)) = stack.pop_front() {
+        if visited.contains(&tile) {
+            continue;
+        }
+        visited.insert(tile);
+        result.push((tile.clone(), distance));
+        for neighbor in tile.neighbors(world) {
+            stack.push_back((neighbor, distance + 1));
+        }
+    }
+    result
+}
+
+pub fn part1(args: &Args) -> u64 {
+    let input = fs::read_to_string(&args.file).expect("I/O error");
+    let world = string_as_2d_array(input);
+    for (y, line) in world.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            if *c == 'S' {
+                let distances = follow_pipe(&world, x, y);
+                let mut max_distance = 0;
+                for (_tile, distance) in distances {
+                    if distance > max_distance {
+                        max_distance = distance;
+                    }
+                }
+                return max_distance;
+            }
+        }
+    }
+    return 0;
+}
+
+fn outside(world: &mut Vec<Vec<char>>, walls: &HashSet<Tile>) -> u64 {
+    let mut stack: VecDeque<(Tile, u64)> = [(Tile::from_world(world, 0, 0), 0)].into();
+    let mut visited: HashSet<Tile> = HashSet::new();
+    let mut count = 0;
+    while let Some((tile, distance)) = stack.pop_front() {
+        if visited.contains(&tile) {
+            continue;
+        }
+        visited.insert(tile);
+        world[tile.y][tile.x] = ' ';
+        count += 1;
+        for t in tile.adjacencies().iter() {
+            if !walls.contains(t) {
+                stack.push_back((t.clone(), distance + 1));
+            }
+        }
+    }
+    count
+}
+
+fn count_inside(world: &mut Vec<Vec<char>>, walls: &HashSet<Tile>) -> u64 {
+    let mut count = 0;
+    for (y, line) in world.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            let tile = Tile::from_world(world, x, y);
+            if walls.contains(&tile) {
+                print!("\x1b[1;32m{}\x1b[0m", c);
+                continue;
+            }
+            let mut t = tile;
+
+            t = tile;
+            let mut left = 0;
+            while let Some(next) = t.left() {
+                if walls.contains(&next) {
+                    let nextc = next.char_at(world).unwrap();
+                    if nextc != '-' && nextc != 'F' && nextc != '7' {
+                        left += 1;
+                    }
+                }
+                t = next;
+            }
+            if left == 0 {
+                print!("{}", c);
+                continue;
+            }
+
+            t = tile;
+            let mut right = 0;
+            while let Some(next) = t.right() {
+                if walls.contains(&next) {
+                    let nextc = next.char_at(world).unwrap();
+                    if nextc != '-' && nextc != 'L' && nextc != 'J' {
+                        right += 1;
+                    }
+                }
+                t = next;
+            }
+            if right == 0 {
+                print!("{}", c);
+                continue;
+            }
+
+            t = tile;
+            let mut bottom = 0;
+            while let Some(next) = t.bottom() {
+                if walls.contains(&next) {
+                    let nextc = next.char_at(world).unwrap();
+                    if nextc != '|' && nextc != 'J' && nextc != '7' {
+                        bottom += 1;
+                    }
+                }
+                t = next;
+            }
+            if bottom == 0 {
+                print!("{}", c);
+                continue;
+            }
+
+            t = tile;
+            let mut top = 0;
+            while let Some(next) = t.top() {
+                if walls.contains(&next) {
+                    let nextc = next.char_at(world).unwrap();
+                    if nextc != '|' && nextc != 'L' && nextc != 'F' {
+                        top += 1;
+                    }
+                }
+                t = next;
+            }
+            if top == 0 {
+                print!("{}", c);
+                continue;
+            }
+
+            if (left % 2) + (right % 2) + (top % 2) + (bottom % 2) < 3 {
+                print!("{}", c);
+                continue;
+            }
+
+            print!("\x1b[1;31m{}\x1b[0m", c);
+            count += 1
+        }
+        print!("\n");
+    }
+    count
+}
+
+pub fn part2(args: &Args) -> u64 {
+    let input = fs::read_to_string(&args.file).expect("I/O error");
+    let mut world = string_as_2d_array(input);
+    let mut expanded_world: Vec<Vec<char>> = vec![];
+    let mut pipe_nodes: HashSet<Tile> = HashSet::new();
+    let w = world[0].len();
+    let h = world.len();
+    expanded_world.push(vec![' '; w + 2]);
+    for (y, line) in world.iter().enumerate() {
+        let mut next_row = vec![' '; w + 2];
+        for (x, c) in line.iter().enumerate() {
+            if *c == 'S' {
+                let distances = follow_pipe(&world, x, y);
+                distances.iter().for_each(|x| {
+                    let mut new_wall = x.0.clone();
+                    new_wall.h += 2;
+                    new_wall.w += 2;
+                    new_wall.x += 1;
+                    new_wall.y += 1;
+                    pipe_nodes.insert(new_wall);
+                });
+            }
+            next_row[x + 1] = *c;
+        }
+        expanded_world.push(next_row);
+    }
+    expanded_world.push(vec![' '; w + 2]);
+    let wall_tiles = pipe_nodes.len();
+    let inside_tiles = count_inside(&mut expanded_world, &pipe_nodes);
+    return inside_tiles;
+}
+
 pub fn entrypoint(args: &Args) {
-    let _input = fs::read_to_string(&args.file).expect("I/O error");
-    println!("this day has not yet been implemented")
+    if args.part2 {
+        let res = part2(args);
+        println!("{}", res);
+    } else {
+        let res = part1(args);
+        println!("{}", res);
+    }
 }
